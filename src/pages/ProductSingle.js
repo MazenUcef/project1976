fd6d71a08a8cfca1d508dd81af37a7fe25bef5a0
MazenY <mazenafifi1999@gmail.com>
2023-12-08 13:16:28 +0200

    last upd

--- src/pages/ProductSingle.js
+++ src/pages/ProductSingle.js
@@ -23,7 +23,9 @@ return (
 <div className='main-product-wrapper py-5 home-wrapper-2'>
     <div className='container-xxl'>
         <div className='row'>
-            <div className='col-6'></div>
+            <div className='col-6'>
+                <div className='main-product-img'></div>
+            </div>
             <div className='col-6'></div>
         </div>
     </div>
